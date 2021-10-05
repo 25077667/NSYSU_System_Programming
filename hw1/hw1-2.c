1585c541ab1c5bc685aa285773e65ef1e7cdd7f5
25077667 <zxc25077667@protonmail.com>
2021-10-05 16:58:52 +0800

    Finish hw1

---
+++ hw1/hw1-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+    if (__glibc_unlikely(argc != 3)) {
+        fprintf(stderr, "DO NOT SUPPORT THIS OPERATION!\n");
+        return 1;
+    }
+
+    FILE *in = fopen(argv[1], "r");
+    FILE *out = fopen(argv[2], "w+");
+    int c;
+    while ((c = fgetc(in)) != EOF)
+        fputc(c, out);
+
+    fclose(in);
+    fclose(out);
+    return 0;
+}
