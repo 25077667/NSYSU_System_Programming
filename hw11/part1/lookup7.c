d098501b1e6aa99d94a5bbb7d96878b9a87aabf2
25077667 <zxc25077667@gmail.com>
2021-12-24 20:48:22 +0800

    Fin part1 of hw11

--- hw11/part1/lookup7.c
+++ hw11/part1/lookup7.c
@@ -4,38 +4,50 @@
  * The name of the socket is passed as resource.
  */
 
-#include <sys/types.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <sys/un.h>
+#include <unistd.h>
 
 #include "dict.h"
 
-int lookup(Dictrec * sought, const char * resource) {
-	static int sockfd;
-	static struct sockaddr_un server;
-	static int first_time = 1;
-	int n;
+static const struct sockaddr_un *ser_addr;
+static const int *ser_fd;
 
-	if (first_time) {     /* connect to socket ; resource is socket name */
-		first_time = 0;
+//! Dirty here but this is the only way to ctor const shared global data.
+static inline void set_addr_(const char *name)
+{
+    struct sockaddr_un sk = {.sun_family = AF_UNIX, .sun_path = {0}};
+    strcpy(sk.sun_path, name);
+    ser_addr = (struct sockaddr_un *) malloc(sizeof(struct sockaddr_un));
+    memcpy((struct sockaddr_un *) ser_addr, &sk, sizeof(sk));
 
-		/* Set up destination address.
-		 * Fill in code. */
+    int sd = socket(AF_UNIX, SOCK_STREAM, 0);
+    ser_fd = (const int *) malloc(sizeof(int));
+    *(int *) ser_fd = sd;
+}
 
-		/* Allocate socket. */
-		server.sun_family = AF_UNIX;
-		strcpy(server.sun_path,resource);
+int my_connect(const char *name)
+{
+    set_addr_(name);
+    const socklen_t len =
+        strlen(ser_addr->sun_path) + sizeof(ser_addr->sun_family);
+    return connect(*ser_fd, (const struct sockaddr *) ser_addr, len);
+}
 
-		/* Connect to the server.
-		 * Fill in code. */
-	}
+int lookup(Dictrec *sought, const char *resource)
+{
+    (void) resource;
+    Wunused(write(*ser_fd, sought->word, strlen(sought->word) + 1));
+    return read(*ser_fd, sought->text, sizeof(sought->text));
+}
 
-	/* write query on socket ; await reply
-	 * Fill in code. */
-
-	if (strcmp(sought->text,"XXXX") != 0) {
-		return FOUND;
-	}
-
-	return NOTFOUND;
+int my_disconnect(void)
+{
+    close(*ser_fd);
+    free((void *) ser_fd);
+    free((void *) ser_addr);
+    return 0;
 }
--- hw11/part1/usock_server.c
+++ hw11/part1/usock_server.c
@@ -5,69 +5,85 @@
  * PORT is defined in dict.h
  */
 
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <sys/un.h>
 #include <errno.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
 #include "dict.h"
 
-int main(int argc, char **argv) {
-    struct sockaddr_un server;
-    int sd,cd,n;
-    Dictrec tryit;
+static int serverId;
+static char sun_path[108];
 
-    if (argc != 3) {
-      fprintf(stderr,"Usage : %s <dictionary source>"
-          "<Socket name>\n",argv[0]);
-      exit(errno);
+static void __clean_serverId(int sig)
+{
+    (void) sig;
+    close(serverId);
+    unlink(sun_path);
+}
+
+static inline void serve_request(int cli_fd, const char *dict_name)
+{
+    char buf[256] = {0};
+    EE(my_connect(dict_name));  // Connect the database first
+    while (read(cli_fd, buf, 256) != EOF) {
+        Dictrec try_it = {.word = {0}, .text = "Not Found!\n"};
+        strcpy(try_it.word, buf);
+        puts(buf);
+        (void) lookup(&try_it, dict_name);
+        Wunused(write(cli_fd, try_it.text, strlen(try_it.text) + 1));
+        memset(buf, 0, 256);
     }
 
-    /* Setup socket.
-     * Fill in code. */
-    
-    /* Initialize address.
-     * Fill in code. */
+    EE(my_disconnect());
+}
 
-    /* Name and activate the socket.
-     * Fill in code. */
+static inline void init_sig_handle()
+{
+    struct sigaction sa = {.sa_flags = 0, .sa_handler = __clean_serverId};
+    sigemptyset(&sa.sa_mask);
+    sigaction(SIGINT, &sa, NULL);
+    sigaction(SIGTERM, &sa, NULL);
+}
 
-    /* main loop : accept connection; fork a child to have dialogue */
-    for (;;) {
-		/* Wait for a connection.
-		 * Fill in code. */
+int main(int argc, char **argv)
+{
+    init_sig_handle();
+    if (argc != 3) {
+        fprintf(stderr,
+                "Usage : %s <dictionary source> "
+                "<Socket name>\n",
+                argv[0]);
+        exit(errno);
+    }
 
-		/* Handle new client in a subprocess. */
-		switch (fork()) {
-			case -1 : 
-				DIE("fork");
-			case 0 :
-				close (sd);	/* Rendezvous socket is for parent only. */
-				/* Get next request.
-				 * Fill in code. */
-				while (___________)) {
+    struct sockaddr_un server = {.sun_family = AF_UNIX, .sun_path = {0}};
+    strcpy(server.sun_path, argv[2]);
+    const int sd = socket(AF_UNIX, SOCK_STREAM, 0);
+    const socklen_t ser_len =
+        strlen(server.sun_path) + sizeof(server.sun_family);
+    EE(bind(sd, (struct sockaddr *) &server, ser_len));
+    EE(listen(sd, 8));
 
-					/* Lookup request. */
-					switch(lookup(&tryit,argv[1]) ) {
-						/* Write response back to client. */
-						case FOUND: 
-							/* Fill in code. */
-							break;
-						case NOTFOUND: 
-							/* Fill in code. */
-							break;
-						case UNAVAIL:
-							DIE(argv[1]);
-					} /* end lookup switch */
+    // Copy local para to global
+    strncpy(sun_path, server.sun_path, sizeof(sun_path));
+    serverId = sd;
 
-				} /* end of client dialog */
+    while (1) {
+        socklen_t ser_len = sizeof(server.sun_family) + strlen(server.sun_path);
+        const int cd = accept(sd, (struct sockaddr *) &server, &ser_len);
+        if (cd == -1)
+            break;
+        /* Handle new client in a subprocess. */
+        if (!fork()) {
+            close(sd); /* Rendezvous socket is for parent only. */
+            serve_request(cd, argv[1]);
+            exit(0);
+        }
+        close(cd);
+    }
 
-				/* Terminate child process.  It is done. */
-				exit(0);
-
-			/* Parent continues here. */
-			default :
-				close(cd);
-				break;
-		} /* end fork switch */
-    } /* end forever loop */
-} /* end main */
+    __clean_serverId(0);
+    return 0;
+}
